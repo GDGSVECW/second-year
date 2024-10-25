3f178a8d7c607e2f595712bfe5e4e18475b7552a
srikavya15 <srikavya.karri15@gmail.com>
2024-10-25 20:46:04 +0530

    Updated argument
     changed argument type to int

--- sumofnumbers.py
+++ sumofnumbers.py
@@ -2,9 +2,9 @@ def sum_of_list(lst):
     total = 0
     for num in lst:
         total = total + n  
-    return Total  
+    return total  
 
 # Test the function
-print(sum_of_list([1, 2, '3', 4])) 
+print(sum_of_list([1, 2, 3, 4])) 
 print(sum_of_list([1, 2, 3, 4, 5]))  
 print(sum_of_list([]))  
