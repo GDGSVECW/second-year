479915d36a83e03ac0a8537dc950648ca65470aa
Sumiya2927 <shaikchandani1107@gmail.com>
2024-10-26 23:03:07 +0530

    Update evenoroddchecker.py
    i changed the assignment operator in if condition, colon beside else and coma after num in print statement

--- evenoroddchecker.py
+++ evenoroddchecker.py
@@ -1,9 +1,8 @@
 def check_even_odd(num):
-    if num % 2 = 0:  
+    if num % 2 == 0:  
         print(f"{num} is even")
-    else
-        print(num + " is odd") 
-    return None
+    else:
+        print(num," is odd") 
 
 check_even_odd(7) 
 check_even_odd(-4)  
