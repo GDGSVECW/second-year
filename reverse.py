7f9b153cce7442dc242e29fc334b021228c4bd32
Sumiya2927 <shaikchandani1107@gmail.com>
2024-10-26 20:23:58 +0530

    Update reverse.py

--- reverse.py
+++ reverse.py
@@ -4,7 +4,6 @@ def reverse_string(s):
         reversed_str = reversed_str + char 
     return reversed_string 
 
-# Test the function
 print(reverse_string("Hacktoberfest"))  
 print(reverse_string(12345))         
 print(reverse_string(""))             
