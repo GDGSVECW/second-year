ae4e1ca8d111462231e014b21f232637382cc877
lathasrikakulapati <lathasribtech@gmail.com>
2024-10-26 09:15:47 +0530

    Update evenoroddchecker.py

--- evenoroddchecker.py
+++ evenoroddchecker.py
@@ -1,10 +1,10 @@
 def check_even_odd(num):
-    if num % 2 = 0:  
+    if num % 2 == 0:  
         print(f"{num} is even")
-    else
-        print(num + " is odd") 
+    else:  # Added colon here
+        print(f"{num} is odd")  # Changed to f-string for consistency
     return None
 
 # Test the function
 check_even_odd(7) 
-check_even_odd(-4)  
+check_even_odd(-4)
